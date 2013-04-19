cdc6ccf3f8099d2c647719e9c4acc6d58342b04d
Rob Sawyer <robksawyer@gmail.com>
2013-04-18 18:38:33 -0700

    Made more datasource changes

--- Model/FoursquareAppModel.php
+++ Model/FoursquareAppModel.php
@@ -4,7 +4,9 @@ App::uses('CakeSession', 'Model/Datasource');
 App::uses('DataSource', 'Model/Datasource');
 App::uses('DboSource', 'Model/Datasource');
 class FoursquareAppModel extends AppModel {
-	public $useDbConfig = 'Foursquare.foursquare';
+
+	public $useDbConfig = 'foursquare';
+	
 }
 
 ?>
