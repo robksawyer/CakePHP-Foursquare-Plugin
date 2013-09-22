4568acaf1449e4c7b4e6834ec9b761e0550bcedc
Rob Sawyer <robksawyer@gmail.com>
2013-09-22 13:15:34 -0700

    Added venue hq url

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -3,7 +3,8 @@ $foursquare = array(
 	'client_id' => getenv('FOURSQUARE_ID'),
 	'client_secret' => getenv('FOURSQUARE_SECRET'),
 	'grant_type' => 'authorization_code', //Leave this as it is
-	'redirect_uri' => getenv('FOURSQUARE_REDIRECT')
+	'redirect_uri' => getenv('FOURSQUARE_REDIRECT'),
+	'venue_url' => 'https://foursquare.com/v/foursquare-hq'
 );
 Configure::write('Foursquare', $foursquare);
 ?>
